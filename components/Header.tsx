5a69bd28ec7b699d60d2fa88ecd62c19e19aceae
Ishpreet-Kaur-Bedi <ishpreetshriya@gmail.com>
2023-08-08 11:42:01 +0530

    Header fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,12 @@
-"use client"
-
-
-
-
 import React from 'react';
-import { SocialIcon } from 'react-social-icons';
+import { AiFillLinkedin,AiFillGithub } from 'react-icons/ai';
+import {BsMedium} from 'react-icons/bs'
+import {BiLogoBlogger} from 'react-icons/bi'
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-type Props = {};
+import { SocialIcon } from 'react-social-icons'; // Import SocialIcon if it's part of your codebase
 
-export default function Header({}: Props) {
+export default function Header() {
   return (
     <header className="sticky p-5 top-0 flex items-center justify-between max-w-7xl mx-auto z-20 xl:items-center">
       {/* Social Icons */}
@@ -29,34 +26,44 @@ export default function Header({}: Props) {
         }}
         className="flex flex-row items-center space-x-4 z-20"
       >
-        {/* socialIcons */}
-        <SocialIcon url="https://www.linkedin.com/in/ishpreet-kaur-bedi/" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="https://www.linkedin.com/in/ishpreet-kaur-bedi/" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="https://www.linkedin.com/in/ishpreet-kaur-bedi/" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="https://www.linkedin.com/in/ishpreet-kaur-bedi/" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="https://www.linkedin.com/in/ishpreet-kaur-bedi/" fgColor="gray" bgColor="transparent" />
+        {/* Wrap the icon with a link */}
+        <a href="https://www.linkedin.com/in/ishpreet-kaur-bedi/" target="_blank" rel="noopener noreferrer">
+          <AiFillLinkedin size={30} color="gray" />
+        </a>
+        <a href="https://github.com/Ishpreet-Kaur-Bedi" target="_blank" rel="noopener noreferrer">
+          <AiFillGithub size={30} color="gray" />
+        </a>
+        <a href="https://medium.com/@Ishpreet_Kaur" target="_blank" rel="noopener noreferrer">
+          <BsMedium size={30} color="gray" />
+        </a>
+        <a href="https://preetsquill.blogspot.com/" target="_blank" rel="noopener noreferrer">
+          <BiLogoBlogger size={30} color="gray" />
+        </a>
+      
+        <a href="https://www.linkedin.com/in/ishpreet-kaur-bedi/" target="_blank" rel="noopener noreferrer">
+          <AiFillLinkedin size={30} color="gray" />
+        </a>
       </motion.div>
 
       {/* Contact */}
-<Link href = "#contact">
-      
-      <motion.div
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1
-        }}
-        transition={{ duration: 1.5 }}
-        className="flex items-center text-gray-300 cursor-pointer z-20"
-      >
+      <Link href="#contact">
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1
+          }}
+          transition={{ duration: 1.5 }}
+          className="flex items-center text-gray-300 cursor-pointer z-20"
+        >
         <SocialIcon network="email" fgColor="gray" bgColor="transparent" className="" />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in touch</p>
-      </motion.div>
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Contact Me</p>
+        </motion.div>
       </Link>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,71 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import About from '@/components/About'
-import Experience from '@/components/Experience'
-import Skills from '@/components/Skills'
-import Projects from '@/components/Projects'
-import ContactMe from '@/components/ContactMe'
-import Link from 'next/link'
-import ClientOnly from '@/components/ClientOnly'
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
+import styled from 'styled-components';
+import Head from 'next/head';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import Experience from '@/components/Experience';
+import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
+import ContactMe from '@/components/ContactMe';
+import Link from 'next/link';
+import ClientOnly from '@/components/ClientOnly';
+
+const FontWrapper = styled.div`
+  font-family: 'Poppins', sans-serif;
+`;
 
 export default function Home() {
   return (
-<div className='bg-[rgb(36,36,36)] text-white  h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7AB0A]/80   '>
+    <FontWrapper>
+      <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7AB0A]/80'>
+        <Head>
+          <title> Ishpreet-Portfolio</title>
+        </Head>
 
-  <Head>
-    <title> Ishpreet-Portfolio</title>
+        <ClientOnly>
+          <Header />
 
-  </Head>
+          <section id='hero' className='snap-start'>
+            {/* <Hero/> */}
+          </section>
 
-  <ClientOnly>
+          {/* About */}
+          <section id='about' className='snap-center'>
+            {/* <About/> */}
+          </section>
+          {/* Experience */}
+          <section id='experience' className='snap-center'>
+            {/* <Experience/> */}
+          </section>
+          {/* Skills */}
+          <section id='skills' className='snap-start'>
+            {/* <Skills/> */}
+          </section>
 
-<Header/>
+          {/* Projects */}
+          <section id='projects' className='snap-start'>
+            {/* <Projects/> */}
+          </section>
 
-{/* hero */}
-<section id='hero' className='snap-start'>
-<Hero/>
-</section>
+          {/* Contact Me */}
+          <section id='contact' className='snap-start'>
+            {/* <ContactMe/> */}
+          </section>
 
-{/* About  */}
-<section id='about' className='snap-center'>
-  <About/>
-</section>
-{/* Experience */}
-<section id='experience' className='snap-center
-'>
-<Experience/>
-</section>
-{/* Skills  */}
-<section id="skills" className=' snap-start'></section>
-<Skills/>
-
-{/* Projects */}
-
-<section id='projects' className='snap-start'>
-<Projects/>
-</section>
-{/* contact me  */}
-
-<section id='contact' className='snap-start'>
-
-  <ContactMe/>
-
-</section>
-
-<Link  href="#hero">
-<footer className='sticky bottom-5 w-full cursor-pointer'>
-  <div className='flex items-center justify-center' >
-    <Image
-    className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
-    src="" alt="" />
-  </div>
-</footer>
-
-</Link>
-    
-</ClientOnly>
-</div>
-
-    
-    
-  )
+          {/* Footer */}
+          {/* <Link href='#hero'>
+            <footer className='sticky bottom-5 w-full cursor-pointer'>
+              <div className='flex items-center justify-center'>
+                <Image
+                  className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+                  src='' alt=''
+                />
+              </div>
+            </footer>
+          </Link> */}
+        </ClientOnly>
+      </div>
+    </FontWrapper>
+  );
 }
