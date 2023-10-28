170193a90c477dda11e855bbf3d4ed79000c775c
ishpreet-kaur-bedi <kaur.ishpreet.bedi@gmail.com>
2023-10-28 19:07:02 +0530

    about page fixed

--- components/About.tsx
+++ components/About.tsx
@@ -10,10 +10,10 @@ export default function About({}: Props) {
         initial={{ opacity: 1 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+        className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-4 md:px-10 justify-evenly mx-auto items-center mt-8 pt-16"
       >
-        <h3 className="absolute top-24 p-5 md:top-0 uppercase tracking-[20px] text-gray-500 text-2xl">
-          About
+        <h3 className="absolute hidden sm:block top-0 p-5 md:top-0 uppercase tracking-[20px] text-gray-500 text-2xl md:relative">
+          
         </h3>
         <motion.img
           initial={{
@@ -24,22 +24,20 @@ export default function About({}: Props) {
           whileInView={{ x: 0, opacity: 1 }}
           viewport={{ once: true }}
           src="/Images/IshpreetProfileImage.jpeg"
-          className="-mb-20 md:-mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px] md:mt-8"
+          className="mt-8 md:-mt-8 md:-ml-8 w-64 h-64 rounded-full object-cover md:rounded-lg xl:w-[500px] xl:h-[600px]"
           alt="Profile Image"
         />
 
-        <div className="space-y-10 px-0 md:px-10 mt-8 md:mt-0">
-          {/* Added a top margin for mobile devices */}
-          <h4 className="text-4xl font-semibold">
+        <div className="mt-8 md:mt-0 space-y-4 md:ml-8">
+          <h4 className="text-3xl md:text-4xl font-semibold">
             Here is a <span className="underline decoration-[#f7AB0A]/50">little</span> background
           </h4>
-          <p className="text-base">
+          <p className="text-base md:text-lg">
             🎓 Computer Science Undergrad at IIITM Gwalior 🖥️ <br />
-            💻 Full Stack Web Developer proficient in MERN stack, Next JS, and Typescript 🌐
-            <br />
-            🔢 Passionate about Data Structures & Algorithms ⚙️<br />
-            🌱 Learning new technologies and embracing creative and practical applications ✨<br />
-            📚 A poet, writer, and avid reader, exploring the power of language to unlock the depths of human experience 🖋️📖<br />
+            💻 Full Stack Web Developer proficient in MERN stack, Next JS, and Typescript 🌐 <br />
+            🔢 Passionate about Data Structures & Algorithms ⚙️ <br />
+            🌱 Learning new technologies and embracing creative and practical applications ✨ <br />
+            📚 A poet, writer, and avid reader, exploring the power of language to unlock the depths of human experience 🖋️📖 <br />
           </p>
         </div>
       </motion.div>
